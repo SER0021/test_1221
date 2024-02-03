61caa79bbf54ca3988ff1bd767f4b275dd36bbc2
SER0021 <ser00021@yandex.ru>
2024-02-03 12:43:08 +0300

    Initial Commit

---
+++ test_1221/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  test_1221
+//
+//  Created by Сергей Васильев on 03.02.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ test_1221/test_1221App.swift
@@ -0,0 +1,17 @@
+//
+//  test_1221App.swift
+//  test_1221
+//
+//  Created by Сергей Васильев on 03.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct test_1221App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
